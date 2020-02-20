f28708b8481beb4ed5a25ae4f48d2f4eaa29397a
namelb <2780257110@qq.com>
2020-02-20 22:24:26 +0800

    新增自动化打包功能

--- tool/bind.js
+++ tool/bind.js
@@ -3,35 +3,82 @@
 const Fs = require('fs')
 const Path = require('path')
 const folders = require(Path.join(__dirname, '../config.json')).folders
+bindForAll()
 
-//#region 打包 index.html 
-try {
-  (function () {
-    console.info('----------打包 index.html----------')
-    const index_html = Fs.readFileSync(Path.join(__dirname, '../public/index.html')).toString()
-    const lines = index_html.split('\n')
-    for (let i = lines.length; i--;) {
-      if (lines[i].indexOf('</html') !== -1) {
-        lines[i] = `<script src="./index.js"></script>\n` + lines[i]
-        break
-      }
-    }
-    const output = lines.join('\n')
-    console.info(output)
-    Fs.writeFileSync(Path.join(__dirname, '../dist/index.html'), output)
-  }())
-} catch (error) {
-  console.error('-------无法读取根目录下的 ./public/index.html 文件')
-  throw error
+let param = process.argv[2] || ''
+if (param && param === 'auto') {
+  autoBind()
 }
-//#endregion
 
-//#region 打包 index.js
-try {
-  console.info('----------打包 index.js----------')
+// 自动打包，监听文件改变自动打包
+function autoBind() {
+  const jsFileList = []
+  folders.forEach(e => getAllJs(e, jsFileList))
+  console.log(jsFileList)
+  const watcher = new FileStatusWatcher(jsFileList)
+  setInterval(() => {
+    if (watcher.isChanged()) {
+      console.info("-----监听到文件改变，自动打包-----")
+      bindForAll()
+    }
+  }, 100)
+}
+// 文件状态监听器
+function FileStatusWatcher(fileList) {
+  this.fileList = fileList.map(e => {
+    return { path: e, stat: Fs.statSync(e) }
+  })
+  this.isChanged = function () {
+    let isChanged = false
+    this.fileList = this.fileList.map(e => {
+      const stat = Fs.statSync(e.path)
+      if (e.stat.mtimeMs !== stat.mtimeMs) {
+        console.log(stat.mtimeMs)
+        isChanged = true
+      }
+      return {
+        path: e.path, stat
+      }
+    })
+    return isChanged
+  }
+}
+// 打包所有文件
+function bindForAll() {
+  try {
+    console.info('----------打包 index.html----------')
+    bindForHtml()
+  } catch (error) {
+    console.error('-------无法读取根目录下的 ./public/index.html 文件')
+    throw error
+  }
+  try {
+    console.info('----------打包 index.js----------')
+    bindForJS()
+  } catch (error) {
+    console.error('-------根目录下的 ./public/index.js 文件 生成失败')
+    throw error
+  }
+}
+// 打包 index.html 
+function bindForHtml() {
+  const index_html = Fs.readFileSync(Path.join(__dirname, '../public/index.html')).toString()
+  const lines = index_html.split('\n')
+  for (let i = lines.length; i--;) {
+    if (lines[i].indexOf('</html') !== -1) {
+      lines[i] = `<script src="./index.js"></script>\n` + lines[i]
+      break
+    }
+  }
+  const output = lines.join('\n')
+  console.info(output)
+  Fs.writeFileSync(Path.join(__dirname, '../dist/index.html'), output)
+}
+// 打包 index.js 
+function bindForJS() {
   const fileList = []
   folders.forEach(e => {
-    getAllJs(Path.join(__dirname, '../', e))
+    getAllJs(Path.join(__dirname, '../', e), fileList)
   })
   try {
     Fs.unlinkSync(Path.join(__dirname, '../dist/index.js'))
@@ -39,30 +86,26 @@ try {
     Fs.writeFileSync(Path.join(__dirname, '../dist/index.js'))
   }
   fileList.forEach(e => {
-    const JS = Fs.readFileSync(e).toString()+'\n'
+    const JS = Fs.readFileSync(e).toString() + '\n'
     console.info(JS)
-    if(JS){
-      Fs.appendFileSync(Path.join(__dirname,'../dist/index.js'),JS)
+    if (JS) {
+      Fs.appendFileSync(Path.join(__dirname, '../dist/index.js'), JS)
     }
   })
-  const JS = Fs.readFileSync(Path.join(__dirname,'../index.js')).toString()+'\n'
+  const JS = Fs.readFileSync(Path.join(__dirname, '../index.js')).toString() + '\n'
   console.info(JS)
-  Fs.appendFileSync(Path.join(__dirname,'../dist/index.js'),JS)
-  // 获取所有JS文件路径
-  function getAllJs(path) {
-    try {
-      const folders = Fs.readdirSync(path)
-      console.info('读取文件夹---' + path)
-      folders.forEach(e => {
-        getAllJs(Path.join(path, e))
-      })
-    } catch (error) {
-      console.info('添加文件路径---' + path)
-      fileList.push(path)
-    }
-  }
-} catch (error) {
-  console.error('-------根目录下的 ./public/index.js 文件 生成失败')
-  throw error
+  Fs.appendFileSync(Path.join(__dirname, '../dist/index.js'), JS)
 }
-//#endregion
+// 获取目录下所有JS文件路径
+function getAllJs(path, list) {
+  try {
+    const folders = Fs.readdirSync(path)
+    console.info('读取文件夹---' + path)
+    folders.forEach(e => {
+      getAllJs(Path.join(path, e), list)
+    })
+  } catch (error) {
+    console.info('添加文件路径---' + path)
+    list.push(path)
+  }
+}
